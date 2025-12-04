523dda0be221c499defc737b7527abd22ef34e15
tonyotero5 <antoniooterodeantonio@gmail.com>
2025-12-04 16:39:44 +0100

    5

--- src/Logica/Robot.java
+++ src/Logica/Robot.java
@@ -1,32 +1,80 @@
 package Logica;
 
-import java.util.Random;
-
 public class Robot {
 	
-	/*public Robot() {
-		vidas = 100;
-	}*/
+	public Robot() {
+		vida = 100;
+	}
 	
+
 	private String nombre;
-	private int vidas;
+	private int vida;	//Valor inicial del número de vidas
 	private Arma arma;
 	
 	//Constructor que recibe nombre y arma
 	public Robot(String nombre, Arma arma) {
 		this.nombre = nombre;
+		setVida();
+	}
+	
+	public Robot(String nombre) {
+		this.nombre = nombre;
+		this.arma = new Arma("Puños de Acero", 10);
+	}
+	
+	//Getters y Setters
+	public int getVida() {
+		return vida;
+	}
+
+	private void setVida() {
+		this.vida = 100;
+		
+	}
+	//-----------------------------------
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Arma getArma() {
+		return arma;
+	}
+
+	public void setArma(Arma arma) {
 		this.arma = arma;
 	}
 	
 	
 	//Métodos
+	
 	void atacar(Robot robot) {
-		Random rand = new Random();
-		int factorSuerte = rand.nextInt(7)+1;
-		//int danioCausado = TipoArma.getPotencia() + factorSuerte;
-		String mensaje; 
+		int factorSuerte = (int) (Math.random() * 8);
+		int danio = arma.getPotencia() + factorSuerte;
+		String mensaje = nombre + " ataca a " + robot.nombre + " con " + arma.getNombre() + "...";
+		System.out.println(mensaje);
+		robot.recibirDanio(danio);
 	}
 	
+	void recibirDanio(int cantidad) {
+        vida -= cantidad;
+        if (vida < 0) {
+        	vida = 0;
+        }
+        System.out.println(" --- " + nombre + " recibe " + cantidad + " puntos de daño. Vida restante: " + vida);
+    }
+	
+	boolean estaVivo() {
+        return vida > 0;
+    }
+	
+	@Override
+	public String toString() {
+		return nombre + " armado con " + arma;
+	}
 	
 }
 
