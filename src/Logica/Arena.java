b3b3daa64edf87141a0b006eb02484e845bafe83
tonyotero5 <antoniooterodeantonio@gmail.com>
2025-12-04 23:44:54 +0100

    Update Arena.java

--- src/Logica/Arena.java
+++ src/Logica/Arena.java
@@ -1,17 +1,26 @@
 package Logica;
 
+import java.util.Random;
+
 public class Arena {
 	Arma[] armas = {
 		new Arma("Motosierra de plasma", 15),
-		new Arma("Motosierra de plasma", 15),
-		new Arma("Motosierra de plasma", 15),
-		new Arma("Motosierra de plasma", 15),
-		new Arma("Motosierra de plasma", 15),
-		new Arma("Motosierra de plasma", 15),
-		new Arma("Motosierra de plasma", 15),
-		new Arma("Motosierra de plasma", 15),
-		new Arma("Motosierra de plasma", 15),
-		new Arma("Motosierra de plasma", 15),
+		new Arma("Cañón de pulsos", 18),
+		new Arma("Espada infinita", 10),
+		new Arma("Martillo neumático", 12),
+		new Arma("Garras de titanio", 10),
+		new Arma("Látigo eléctrico", 11),
+		new Arma("Pistola nuclear", 12),
+		new Arma("Lanza de diamante", 11),
+		new Arma("Escopeta sónica", 14),
+		new Arma("Lanzallamas fundente", 15),
+		new Arma("Puños de acero", 10),
 	};
+	
+	public static Arma obtenerArmaAleatoria(Arma[] armas) {
+        Random aleatorio = new Random();
+        int indice = aleatorio.nextInt(armas.length);
+        return armas[indice];
+    }
 
 }
