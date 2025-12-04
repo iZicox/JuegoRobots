4ea22756d7c3d75dea87022842177f537312a549
tonyotero5 <antoniooterodeantonio@gmail.com>
2025-12-04 15:54:15 +0100

    ll

--- src/Logica/Robot.java
+++ src/Logica/Robot.java
@@ -1,5 +1,32 @@
 package Logica;
 
-public class Robot {
+import java.util.Random;
 
+public class Robot {
+	
+	/*public Robot() {
+		vidas = 100;
+	}*/
+	
+	private String nombre;
+	private int vidas;
+	private Arma arma;
+	
+	//Constructor que recibe nombre y arma
+	public Robot(String nombre, Arma arma) {
+		this.nombre = nombre;
+		this.arma = arma;
+	}
+	
+	
+	//Métodos
+	void atacar(Robot robot) {
+		Random rand = new Random();
+		int factorSuerte = rand.nextInt(7)+1;
+		//int danioCausado = TipoArma.getPotencia() + factorSuerte;
+		String mensaje; 
+	}
+	
+	
 }
+
