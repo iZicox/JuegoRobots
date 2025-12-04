4fa0b82b5e6a99cfe4ea7cee8c3715955ae7c29c
tonyotero5 <antoniooterodeantonio@gmail.com>
2025-12-04 23:33:04 +0100

    Creación del Array 'armas'

--- src/Logica/Arena.java
+++ src/Logica/Arena.java
@@ -1,5 +1,17 @@
 package Logica;
 
 public class Arena {
+	Arma[] armas = {
+		new Arma("Motosierra de plasma", 15),
+		new Arma("Motosierra de plasma", 15),
+		new Arma("Motosierra de plasma", 15),
+		new Arma("Motosierra de plasma", 15),
+		new Arma("Motosierra de plasma", 15),
+		new Arma("Motosierra de plasma", 15),
+		new Arma("Motosierra de plasma", 15),
+		new Arma("Motosierra de plasma", 15),
+		new Arma("Motosierra de plasma", 15),
+		new Arma("Motosierra de plasma", 15),
+	};
 
 }
