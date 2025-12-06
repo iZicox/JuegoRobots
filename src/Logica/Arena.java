e19727d64876b276d5a79f7f4afec28b1acced50
iZicox <53028086+iZicox@users.noreply.github.com>
2025-12-06 22:36:44 +0100

    ajuste de nombres de variable robot
    robot_1 a robot1
    robot_2 a robot2

--- src/Logica/Arena.java
+++ src/Logica/Arena.java
@@ -67,82 +67,81 @@ public class Arena {
 		
 		int indiceArmaRobot1 = obtenerArmaAleatoria(armas);
 		Arma armaRobot1 = new Arma(armas[indiceArmaRobot1]);
-		Robot robot_1 = new Robot(nombreRobot1,armaRobot1);
+		Robot robot1 = new Robot(nombreRobot1,armaRobot1);
 		armas[indiceArmaRobot1] = null;
 		
 		int indiceArmaRobot2 = obtenerArmaAleatoria(armas);
 		Arma armaRobot2 = new Arma(armas[indiceArmaRobot2]);
-		Robot robot_2 = new Robot(nombreRobot2,armaRobot2);
+		Robot robot2 = new Robot(nombreRobot2,armaRobot2);
 		armas[indiceArmaRobot2] = null;
 		
-		System.out.println("Esta noche tenemos por un lado al mítico " + robot_1.getNombre() + " armado con " + robot_1.getArma().getNombre());
-		System.out.println("Y por otro lado contamos con el aspirante invicto " + robot_2.getNombre() + " armado con " + robot_2.getArma().getNombre());
+		System.out.println("Esta noche tenemos por un lado al mítico " + robot1.getNombre() + " armado con " + robot1.getArma().getNombre());
+		System.out.println("Y por otro lado contamos con el aspirante invicto " + robot2.getNombre() + " armado con " + robot2.getArma().getNombre());
 		System.out.println("¡Comienza el combate!");
-		System.out.println(robot_1.getNombre() + " VS " + robot_2.getNombre());
+		System.out.println(robot1.getNombre() + " VS " + robot2.getNombre());
 		System.out.println("-------------------------------------------------");
 		//sorteo de quien inicia
 		System.out.println("Lancemos una moneda a ver quien inicia esta pelea del siglo!!!");
-		System.out.printf("Cara para %s y sello para %s . . .\n",robot_1.getNombre(),robot_2.getNombre());
+		System.out.printf("Cara para %s y sello para %s . . .\n",robot1.getNombre(),robot2.getNombre());
 		System.out.println("Lanzando moneda al aire . . .");
 		if(Util.lanzarMoneda()) {
-			System.out.printf("Ha caido CARA. Empieza %s.\n",robot_1.getNombre());
-			combatePorRondas(sc, robot_1, robot_2);
+			System.out.printf("Ha caido CARA. Empieza %s.\n",robot1.getNombre());
+			combatePorRondas(sc, robot1, robot2);
 		}else {
-			System.out.printf("Ha caido CRUZ. Empieza %s.\n",robot_2.getNombre());
-			combatePorRondas(sc, robot_2, robot_1);
+			System.out.printf("Ha caido CRUZ. Empieza %s.\n",robot2.getNombre());
+			combatePorRondas(sc, robot2, robot1);
 		}
 		
-		ganador(robot_1, robot_2);
+		ganador(robot1, robot2);
 		
 		
 	}
 	
 
-	//Combate. El robot_1 inicia el combate
 	/**
 	 * Controla el combate por rondas entre dos robots. El robot indicado como
-	 * robot_1 ataca primero, seguido por robot_2, alternando turnos hasta que uno
+	 * robot1 ataca primero, seguido por robot2, alternando turnos hasta que uno
 	 * de los dos deja de estar vivo.
 	 *
 	 * @param teclado Scanner para pausar la ejecución esperando pulsación de ENTER.
-	 * @param robot_1 Robot que inicia atacando en la primera ronda.
-	 * @param robot_2 Robot que recibe el primer ataque.
+	 * @param robot1 Robot que inicia atacando en la primera ronda.
+	 * @param robot2 Robot que recibe el primer ataque.
 	 */
 	
-	private static void combatePorRondas(Scanner teclado, Robot robot_1, Robot robot_2) {
+	private static void combatePorRondas(Scanner teclado, Robot robot1, Robot robot2) {
 		
-		while (Util.ambosVivos(robot_1, robot_2)) {
+		while (Util.ambosVivos(robot1, robot2)) {
 
 			System.out.print("Pulsa ENTER para continuar.");
 			teclado.nextLine();
 
 			// robot1 ataca primero
-			robot_1.atacar(robot_2);
+			robot1.atacar(robot2);
 
 			//verifica si alguien murio
-			if (!Util.ambosVivos(robot_1, robot_2)) {
+			if (!Util.ambosVivos(robot1, robot2)) {
 				break;
 			}
 			System.out.print("Pulsa ENTER para continuar.");
 			teclado.nextLine();
 			
 			//robot2 continua atacando
-			robot_2.atacar(robot_1);
+			robot2.atacar(robot1);
 			
 			//verifica si alguien murio
-			if (!Util.ambosVivos(robot_1, robot_2)) {
+			if (!Util.ambosVivos(robot1, robot2)) {
 				break;
 			}
 		}
 	}
 	
 	//Ganador/*
-	private static void ganador(Robot robot_1, Robot robot_2) {
+	private static void ganador(Robot robot1, Robot robot2) {
 		System.out.println("=== FIN DEL COMBATE ===");
-        if (robot_1.estaVivo()) {
-            System.out.println("¡El ganador es " + robot_1.getNombre() + "!");
+        if (robot1.estaVivo()) {
+            System.out.println("¡El ganador es " + robot1.getNombre() + "!");
         } else {
-            System.out.println("¡El ganador es " + robot_2.getNombre() + "!");
+            System.out.println("¡El ganador es " + robot2.getNombre() + "!");
         }
 	}
 	
